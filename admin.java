77f8629cf01923e1f4109a8c37ae2f72725fe365
imam ahdy sabilla <112673843+Imamahdysabilla@users.noreply.github.com>
2023-11-29 21:38:39 +0700

    Create admin.java

---
+++ admin.java
@@ -0,0 +1,46 @@
+package tugaspbokelompok_11;
+public class Admin {
+    private int idAdmin;
+    private String namaAdmin;
+    private String peran;
+
+    public Admin(int idAdmin, String namaAdmin, String peran) {
+        this.idAdmin = idAdmin;
+        this.namaAdmin = namaAdmin;
+        this.peran = peran;
+    }
+
+    public int getIdAdmin() {
+        return idAdmin;
+    }
+
+    public void setIdAdmin(int idAdmin) {
+        this.idAdmin = idAdmin;
+    }
+
+    public String getNamaAdmin() {
+        return namaAdmin;
+    }
+
+    public void setNamaAdmin(String namaAdmin) {
+        this.namaAdmin = namaAdmin;
+    }
+
+    public String getPeran() {
+        return peran;
+    }
+
+    public void setPeran(String peran) {
+        this.peran = peran;
+    }
+    
+    public void tambahAnggota(){
+        
+    }
+    public void tambahBuku(){
+        
+    }
+    public void kelolaTransaksi(){
+        
+    }
+}
