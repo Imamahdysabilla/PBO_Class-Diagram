0fe6f89094728550db2b33b0d03af0c6305d2a66
imam ahdy sabilla <112673843+Imamahdysabilla@users.noreply.github.com>
2023-11-29 21:39:42 +0700

    Create Notifikasi.java

---
+++ Notifikasi.java
@@ -0,0 +1,30 @@
+package tugaspbokelompok_11;
+public class Notifikasi {
+    private int idNotifikasi;
+    private String isiNotifikasi;
+
+    public Notifikasi(int idNotifikasi, String isiNotifikasi) {
+        this.idNotifikasi = idNotifikasi;
+        this.isiNotifikasi = isiNotifikasi;
+    }
+
+    public int getIdNotifikasi() {
+        return idNotifikasi;
+    }
+
+    public void setIdNotifikasi(int idNotifikasi) {
+        this.idNotifikasi = idNotifikasi;
+    }
+
+    public String getIsiNotifikasi() {
+        return isiNotifikasi;
+    }
+
+    public void setIsiNotifikasi(String isiNotifikasi) {
+        this.isiNotifikasi = isiNotifikasi;
+    }
+    
+    public void kirimNotifikasi(){
+        
+    }
+}
